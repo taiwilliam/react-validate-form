35e9c7f2ba39f7e7b774aeeb6eeaf1044982122d
William Tai <44943338+taiwilliam@users.noreply.github.com>
2023-05-19 16:51:50 +0800

    step2 vaildate and catch error message

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 function App() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, formState } = useForm();
   const [data, setData] = useState({});
 
   const onSubmit = (formData) => {
     setData(JSON.stringify(formData));
     console.log(data); // {name: ''}
   };
+
+  console.log(formState); // 獲取錯誤訊息
 
   return (
     <div className='App'>
@@ -18,7 +20,11 @@ function App() {
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className=''>
             <label htmlFor='name'>姓名</label>
-            <input type='text' id='name' {...register("name")} />
+            <input
+              type='text'
+              id='name'
+              {...register("name", { required: true })}
+            />
           </div>
 
           <button type='submit'>傳送</button>
